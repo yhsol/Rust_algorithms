b2f2b7e370387c25e5ec10fbbd5f7ba8af4c3d46
loshy <loshy244110@gmail.com>
2020-05-27 22:13:14 +0900

    feat: abs_sign

---
+++ src/abs_sign.rs
@@ -0,0 +1,7 @@
+pub fn run(a: i32) -> i32 {
+    if a >= 0 {
+        return a;
+    } else {
+        return -a;
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod abs_sign;
+
 fn main() {
     println!("Hello, world!");
+    println!("{}", abs_sign::run(-5))
 }
